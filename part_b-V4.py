7916e08ae98207ac4f95fb3bb80cb16ee4ddf48c
Shantanu Awasthi <sa11g19@soton.ac.uk>
2023-05-16 14:27:28 +0100

    multi_class V4

---
+++ part_b-V4.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, f1_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+# Step 1: Data Preparation
+training_data = pd.read_csv("TrainingDataMulti.csv")
+testing_data = pd.read_csv("TestingDataMulti.csv")
+
+X_train = training_data.iloc[:, :-1]  # Features (input variables) for training data
+y_train = training_data.iloc[:, -1]   # Labels for training data
+
+X_test = testing_data  # Features (input variables) for testing data
+
+# Step 3: Model Selection and Training
+model = LogisticRegression(random_state=0, max_iter=10000)
+model.fit(X_train, y_train)
+
+# Step 4: Compute the Test-training split
+n_samples = len(X_train)
+ratio = 0.95
+X_train_split = X_train[:int(ratio * n_samples)]
+y_train_split = y_train[:int(ratio * n_samples)]
+X_test_split = X_train[int(ratio * n_samples):]
+y_test_split = y_train[int(ratio * n_samples):]
+
+# Step 4: Testing Data Prediction and Output
+predicted_split_labels = model.predict(X_test_split)
+
+# Create a DataFrame for the predicted labels
+testing_split_results = pd.DataFrame(X_test_split)
+testing_split_results['marker'] = predicted_split_labels
+
+# Save the results to a CSV file
+testing_split_results.to_csv("TestingResultsMulti.csv", index=False)
+
+# Calculate F1 score
+f1_split = f1_score(y_train_split, model.predict(X_train_split), average='weighted')
+
+# Calculate confusion matrix
+cm_split = confusion_matrix(y_train_split, model.predict(X_train_split))
+
+# Plot the confusion matrix
+plt.imshow(cm_split, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.colorbar()
+tick_marks = np.arange(3)
+plt.xticks(tick_marks, ['Normal', 'Data Injection', 'Command Injection'])
+plt.yticks(tick_marks, ['Normal', 'Data Injection', 'Command Injection'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+
+# Add labels to each cell
+for i in range(cm_split.shape[0]):
+    for j in range(cm_split.shape[1]):
+        plt.text(j, i, format(cm_split[i, j]), ha='center', va='center')
+
+plt.show()
+
+print("F1 Score:", f1_split)
